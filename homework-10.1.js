407d6d31ae4fda43b00ddda6d77b2c03d566f13a
Shari Evans <shari.evans@comcast.net>
2013-03-25 21:57:20 -0400

    addition for red to green

---
+++ homework-10.1.js
@@ -0,0 +1,36 @@
+var assert = require("assert");
+
+//assert(1 + 1 == 1, '1 + 1 does not equal 1');
+//assert.equal(1, 1 + 1, '1 + 1 does not equal 1');
+
+/**
+ * four functions with two parameters:
+ * - addition
+ * - subtraction
+ * - multiplication
+ * - division
+ * 
+ * Must accept positive and negative Numbers (int or 
+ * float) AND zero. Must not make an error!
+ */
+ 
+/**
+ * testing 
+ * one | two 
+ * ====+=====
+ *  +1 | +1  
+ *  +1 | +0  
+ *  +1 | -1 
+ *  -1 | +1 
+ *  -1 | +0 
+ *  -1 | -1 
+ *  +0 | +1
+ *  +0 | +0
+ *  +0 | -1
+ */
+ 
+ function addition (a,b) {
+     return 2;
+ }
+ 
+ assert.equal(2, addition (1,1));
