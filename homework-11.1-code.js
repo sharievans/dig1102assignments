576bdf767ded5f1f85a7c9c729fb3a1f04f99ae5
Shari Evans <shari.evans@comcast.net>
2013-04-01 21:52:54 -0400

    division red to green

---
+++ homework-11.1-code.js
@@ -0,0 +1,11 @@
+module.exports = {
+    /*
+    something: function(){
+        return 'something';
+    }
+    */
+    
+    division: function division( a, b ) {
+        return 1;
+    }
+}
---
+++ homework-11.1-tests.js
@@ -0,0 +1,17 @@
+var assert = require('assert'),
+    calculator = require('./homework-11.1-code.js');
+    
+    /**
+ * Tests for division...
+ * +1 | +1 | +1
+ * +1 | +0 | +0
+ * +1 | -1 | -1
+ * -1 | +1 | -1
+ * -1 | +0 | +0
+ * -1 | -1 | +1
+ * +0 | +1 | +0
+ * +0 | +0 | +0
+ * +0 | -1 | +0
+ */
+ 
+ assert( calculator.division (1, 1) === 1);
