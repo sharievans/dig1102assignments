d3e811970542ab2a859075b45aaf3cf03726b9ba
Shari Evans <shari.evans@comcast.net>
2013-04-18 19:02:03 -0400

    added 10 closed doors

--- doors-tests.js
+++ doors-tests.js
@@ -6,5 +6,5 @@ assert( hundredDoors.hallway );
 // test: do we have Doors in our Hallway?
 assert( hundredDoors.hallway.doors );
 // test: do we have 100 Doors?
-
+assert( hundredDoors.hallway.doors.length, 10 );
 // test: are all the Doors "closed"?
