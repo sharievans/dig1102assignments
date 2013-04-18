135ac16317ae7071126fe2c7f48708aca2728cd1
Shari Evans <shari.evans@comcast.net>
2013-04-18 19:14:31 -0400

    toggle added

--- doors-tests.js
+++ doors-tests.js
@@ -3,11 +3,17 @@ hundredDoors = require("./doors-code");
 
 // test: do we have a Hallway?
 assert( hundredDoors.hallway );
+
 // test: do we have Doors in our Hallway?
 assert( hundredDoors.hallway.doors );
+
 // test: do we have 100 Doors?
 assert.equal( hundredDoors.hallway.doors.length, 10 );
+
 // test: are all the Doors "closed"?
 hundredDoors.hallway.doors.forEach( function( door ) {
     assert.equal( door, 0 );
 });
+
+// do: PASS 1 -- take a walk down the hallway, change the state of the doors
+assert( hundredDoors.hallway.toggle );
