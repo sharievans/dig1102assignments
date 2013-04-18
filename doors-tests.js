384437383d43f8db708851fc11a6a58cd048b722
Shari Evans <shari.evans@comcast.net>
2013-04-18 19:08:13 -0400

    confirmed the doors are closed

--- doors-tests.js
+++ doors-tests.js
@@ -6,5 +6,8 @@ assert( hundredDoors.hallway );
 // test: do we have Doors in our Hallway?
 assert( hundredDoors.hallway.doors );
 // test: do we have 100 Doors?
-assert( hundredDoors.hallway.doors.length, 10 );
+assert.equal( hundredDoors.hallway.doors.length, 10 );
 // test: are all the Doors "closed"?
+hundredDoors.hallway.doors.forEach( function( door ) {
+    assert.equal( door, 0 );
+});
