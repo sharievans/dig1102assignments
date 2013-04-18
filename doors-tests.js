5f693bc2fc1a50870f79046b903488fe4bf446f5
Shari Evans <shari.evans@comcast.net>
2013-04-17 21:20:49 -0400

    not quite green yet

---
+++ doors-tests.js
@@ -0,0 +1,10 @@
+var assert = require("assert"),
+hundredDoors = require("./doors-code");
+
+// test: do we have a Hallway?
+assert( hundredDoors.hallway );
+// test: do we have Doors in our Hallway?
+
+// test: do we have 100 Doors?
+
+// test: are all the Doors "closed"?
