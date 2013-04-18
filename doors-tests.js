a67defd593c6030fccdcac24e0d1388cd8e4cfc7
Shari Evans <shari.evans@comcast.net>
2013-04-18 18:52:16 -0400

    Added doors

--- doors-tests.js
+++ doors-tests.js
@@ -4,7 +4,7 @@ hundredDoors = require("./doors-code");
 // test: do we have a Hallway?
 assert( hundredDoors.hallway );
 // test: do we have Doors in our Hallway?
-
+assert( hundredDoors.hallway.doors );
 // test: do we have 100 Doors?
 
 // test: are all the Doors "closed"?
