a4cee4ca584fd767bb41e7f705e62ed309ce7b47
Shari Evans <shari.evans@comcast.net>
2013-04-23 08:24:22 -0400

    the doors are closed

--- doors-tests.js
+++ doors-tests.js
@@ -3,3 +3,18 @@ var assert = require( "assert" ),
     
 //do we have a hallway?
 assert( hundredDoors.hallway );
+
+//do we have doors in the hallway?
+assert( hundredDoors.hallway.doors );
+
+//do we have 10 doors?
+assert.equal( hundredDoors.hallway.doors.length, 10 );
+
+//are all the doors closed?
+hundredDoors.hallway.doors.forEach( function (door) {
+    assert.equal( door, 0 );
+});
+
+//can we change the state of a door(create toggle function)?
+// can we change closed to open?
+// can we change open to closed?
